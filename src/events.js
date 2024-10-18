5032236c7656b6c034ad8262892068081d164178
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-10-18 09:43:11 +0300

    add boards to page

--- src/events.js
+++ src/events.js
@@ -1,2 +1,35 @@
 import { Ship, Gameboard, Player } from "./script.js";
 import "./styles.css";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  generateBoards();
+});
+
+function generateBoards() {
+  const playerBoard = document.querySelector(".player-board");
+  const computerBoard = document.querySelector(".computer-board");
+  const player = new Player();
+  const computer = new Player();
+  
+  // player board's grid
+  for (let i = 0; i < player.gameboard.board.length; i++) {
+    for (let j = 0; j < player.gameboard.board[i].length; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+    
+        playerBoard.appendChild(box);
+    }
+  }
+  // computer board's grid
+  for (let i = 0; i < computer.gameboard.board.length; i++) {
+    for (let j = 0; j < player.gameboard.board[i].length; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+    
+        computerBoard.appendChild(box);
+    }
+  }
+  
+
+  
+}

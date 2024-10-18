ffb475ba3ff8f9390ee4159e153adefe026a0faa
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-10-18 10:32:39 +0300

    show ships on board

--- src/events.js
+++ src/events.js
@@ -1,35 +1,84 @@
-import { Ship, Gameboard, Player } from "./script.js";
+import { Ship, Player } from "./script.js";
+
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  generateBoards();
-});
-
-function generateBoards() {
-  const playerBoard = document.querySelector(".player-board");
-  const computerBoard = document.querySelector(".computer-board");
   const player = new Player();
   const computer = new Player();
   
-  // player board's grid
+  createShips(player, computer);  
+  generateBoards(player, computer);  
+});
+
+function generateBoards(player, computer) {
+  const playerBoard = document.querySelector(".player-board");
+  const computerBoard = document.querySelector(".computer-board");
+
+  // Player board's grid
   for (let i = 0; i < player.gameboard.board.length; i++) {
     for (let j = 0; j < player.gameboard.board[i].length; j++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-    
-        playerBoard.appendChild(box);
+      const box = document.createElement("div");
+      box.classList.add("box");
+
+      // Check if there's a ship at the current coordinates
+      if (player.gameboard.board[i][j] instanceof Ship) {
+        box.classList.add("ship");
+      }
+
+      playerBoard.appendChild(box);
     }
   }
-  // computer board's grid
+
+  // Computer board's grid
   for (let i = 0; i < computer.gameboard.board.length; i++) {
-    for (let j = 0; j < player.gameboard.board[i].length; j++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-    
-        computerBoard.appendChild(box);
+    for (let j = 0; j < computer.gameboard.board[i].length; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+
+      // Check if there's a ship at the current coordinates
+      if (computer.gameboard.board[i][j] instanceof Ship) {
+        box.classList.add("ship");
+      }
+
+      computerBoard.appendChild(box);
     }
   }
+}
+
+function createShips(player, computer) {
   
+  const ship1 = new Ship(5, 0, false);
+  const ship2 = new Ship(3, 0, false);
+  const ship3 = new Ship(3, 0, false);
+  const ship4 = new Ship(2, 0, false);
+  const ship5 = new Ship(2, 0, false);
+  const ship6 = new Ship(2, 0, false);
+  const ship7 = new Ship(1, 0, false);
+  const ship8 = new Ship(1, 0, false);
+  const ship9 = new Ship(1, 0, false);
+  const ship10 = new Ship(1, 0, false);
 
   
+  player.gameboard.placeShip(ship1);
+  player.gameboard.placeShip(ship2);
+  player.gameboard.placeShip(ship3);
+  player.gameboard.placeShip(ship4);
+  player.gameboard.placeShip(ship5);
+  player.gameboard.placeShip(ship6);
+  player.gameboard.placeShip(ship7);
+  player.gameboard.placeShip(ship8);
+  player.gameboard.placeShip(ship9);
+  player.gameboard.placeShip(ship10);
+
+  
+  computer.gameboard.placeShip(ship1);
+  computer.gameboard.placeShip(ship2);
+  computer.gameboard.placeShip(ship3);
+  computer.gameboard.placeShip(ship4);
+  computer.gameboard.placeShip(ship5);
+  computer.gameboard.placeShip(ship6);
+  computer.gameboard.placeShip(ship7);
+  computer.gameboard.placeShip(ship8);
+  computer.gameboard.placeShip(ship9);
+  computer.gameboard.placeShip(ship10);
 }
--- src/script.js
+++ src/script.js
@@ -1,3 +1,81 @@
+class Gameboard {
+  constructor() {
+    this.board = Array.from({ length: 10 }, () => Array(10).fill(""));
+    this.misses = [];
+    this.ships = [];
+  }
+
+  getCoords(max) {
+    const x = Math.floor(Math.random() * max);
+    const y = Math.floor(Math.random() * max);
+    return [x, y];
+  }
+
+  randomDirection() {
+    return Math.random() < 0.5 ? "horizontal" : "vertical";
+  }
+
+  placeShip(ship) {
+    let [x, y] = this.getCoords(this.board.length);
+    const direction = this.randomDirection();
+
+    if (!this.validateShipPlacement(x, y, direction, ship.length)) {
+      return this.placeShip(ship); // Retry if invalid placement
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      this.board[x][y] = ship;
+      if (direction === "horizontal") {
+        x++; // Move right for horizontal placement
+      } else {
+        y++; // Move down for vertical placement
+      }
+    }
+
+    this.ships.push(ship);
+  }
+
+  validateShipPlacement(x, y, direction, length) {
+    // Check bounds for horizontal placement
+    if (direction === "horizontal" && x + length > this.board.length) {
+      return false;
+    }
+    // Check bounds for vertical placement
+    if (direction === "vertical" && y + length > this.board[0].length) {
+      return false;
+    }
+    // Check if any space is already occupied
+    for (let i = 0; i < length; i++) {
+      if (this.board[x][y] !== "") {
+        return false;
+      }
+      if (direction === "horizontal") {
+        x++; // Check the next horizontal space
+      } else {
+        y++; // Check the next vertical space
+      }
+    }
+    return true;
+  }
+
+  receiveAttack(coords) {
+    const [x, y] = coords;
+    if (this.board[x][y] instanceof Ship) {
+      this.board[x][y].hit();
+      return "hit";
+    } else {
+      this.misses.push(coords);
+      return "miss";
+    }
+  }
+
+  checkAllShips() {
+    return this.ships.every((ship) => ship.isSunk())
+      ? "game over"
+      : "still playing";
+  }
+}
+
 class Ship {
   constructor(length, timesHit, sunk) {
     this.length = length;
@@ -17,102 +95,10 @@ class Ship {
   }
 }
 
-class Gameboard {
-  constructor() {
-    this.board = [
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", "", "", ""],
-    ];
-    this.misses = [];
-    this.ships = [];
-  }
-
-  getCoords(max) {
-    const x = Math.floor(Math.random() * max);
-    const y = Math.floor(Math.random() * max);
-
-    return [x, y];
-  }
-
-  randomDirection() {
-    if (Math.round(Math.random()) === 0) {
-      return "horizontal";
-    } else {
-      return "vertical";
-    }
-  }
-
-  placeShip(ship) {
-    const boardLength = this.board.length;
-    let coords = this.getCoords(boardLength);
-    const direction = this.randomDirection();
-    let x = coords[0];
-    let y = coords[1];
-
-    let board = this.board;
-    const length = ship.length;
-
-    if (x > board.length || y > board.length) {
-      // Get new coordinates if it is off the board
-      coords = this.getCoords(boardLength);
-    }
-    if (direction === "horizontal") {
-      for (let i = 0; i < length; i++) {
-        if (board[x][y] === 1) {
-          return "there is a ship there";
-        } else {
-          board[x][y] = ship;
-          x++;
-        }
-      }
-    } else {
-      for (let i = 0; i < length; i++) {
-        if (board[x][y] === 1) {
-          return "there is a ship there";
-        } else {
-          board[x][y] = ship;
-          y++;
-        }
-      }
-    }
-    this.ships.push(ship);
-    return board;
-  }
-
-  receiveAttack(coords) {
-    const x = coords[0];
-    const y = coords[1];
-    const board = this.board;
-    if (board[x][y] instanceof Ship) {
-      board[x][y].hit().isSunk();
-      return "hit";
-    } else {
-      this.misses.push(coords);
-      return "miss";
-    }
-  }
-
-  checkAllShips() {
-    if (this.ships.every((ship) => ship.isSunk())) {
-      return "game over";
-    } else {
-      return "still playing";
-    }
-  }
-}
-
 class Player {
   constructor() {
     this.gameboard = new Gameboard();
   }
 }
 
-export { Ship, Gameboard, Player };
+export { Player, Gameboard, Ship };
